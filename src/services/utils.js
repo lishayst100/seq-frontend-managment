3433c60887fec3d5bcc9b2bffb4fb0fedffcbe61
lishayst100 <lishayst100@gmail.com>
2024-01-19 16:07:31 +0200

    real server test

--- src/services/utils.js
+++ src/services/utils.js
@@ -13,9 +13,12 @@ export function convertToBase64(file){
 
 
 //export const BASE_URL = 'https://server-seq.onrender.com/api/projects' 
-export const BASE_URL = 'http://localhost:3001/api/projects' 
-export const BASE_URL_CAROUSEL = 'http://localhost:3001/api/carousel' 
-export const BASE_URL_TEAM = 'http://localhost:3001/api/team' 
+//export const BASE_URL = 'http://localhost:3001/api/projects' 
+//export const BASE_URL_CAROUSEL = 'http://localhost:3001/api/carousel' 
+//export const BASE_URL_TEAM = 'http://localhost:3001/api/team' 
+export const BASE_URL = 'https://server-seq.onrender.com/api/projects' 
+export const BASE_URL_CAROUSEL = 'https://server-seq.onrender.com/api/carousel' 
+export const BASE_URL_TEAM = 'https://server-seq.onrender.com/api/team' 
 
 //http://localhost:3001/api/projects
 //https://brave-cow-headscarf.cyclic.app/api/projects
