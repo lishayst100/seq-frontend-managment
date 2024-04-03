f54616089daff679f4ef5aa5b26b96cb6e75c828
lishayst100 <lishayst100@gmail.com>
2024-04-03 17:54:45 +0300

    url upadte

--- src/services/utils.js
+++ src/services/utils.js
@@ -21,10 +21,10 @@ export function convertToBase64(file){
   }
 
 
-//export const BASE_URL = 'https://server-seq.onrender.com' 
+export const BASE_URL = 'https://server-seq.onrender.com' 
 
 
-export const BASE_URL = 'http://localhost:3001' 
+//export const BASE_URL = 'http://localhost:3001' 
 
 /* export const BASE_URL = 'https://server-seq.onrender.com' 
 export const BASE_URL_CAROUSEL = 'https://server-seq.onrender.com/api/carousel' 
